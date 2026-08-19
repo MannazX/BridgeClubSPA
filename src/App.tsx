db4c8301a31c8a76280084fcdc341cad0cfec9ca
Magnus Sverdrup <magger254@gmail.com>
2026-08-19 10:48:22 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { Routes, Route, Link } from "react-router-dom";
+
+export default function App() {
+    return (
+      <div>
+        <div className="jumbotron pb-3 pt-3">
+          <div className="nav navbar-expand-lg">
+            <nav className="nav navbar-nav">
+
+            </nav>
+          </div>
+        </div>
+      </div>
+    )
+}
