dd61234825a682909c5c7c4c0933c3f4556a6e6d
Magnus Sverdrup <magger254@gmail.com>
2026-08-20 11:51:33 +0200

    table included and interface adjusted

--- src/components/memberDetails.tsx
+++ src/components/memberDetails.tsx
@@ -2,6 +2,25 @@ import { type Member } from "../interfaces/member"
 
 export default function MemberDetails({member}: {member : Member}) {
     return (
-        <div key={member.memberId}>Name: {member.name}</div>
+        <div key={member.membeR_ID}>
+            <table className="table table-primary">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Navn</th>
+                        <th scope="col">Telefon</th>
+                        <th scope="col">Fødselsdato</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{member.membeR_ID}</td>
+                        <td>{member.name}</td>
+                        <td>{member.phonE_1}</td>
+                        <td>{member.birthday instanceof Date ? member.birthday.toLocaleDateString() : member.birthday}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     )
 }
